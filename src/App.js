bfca98681686de156a38eb57fe6b385b615a17a2
BabichaDevi <thbabicha@gmail.com>
2023-09-25 08:22:38 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import Navbar from "./components/Navbar";
+import Filter  from "./components/Filter";
+import Cards from "./components/Cards";
+import Spinner from "./components/Spinner";
+
+import { filterData,apiUrl } from "./data";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+
+const App = () => {
+  const [courses,setCourses]=useState(null);
+  const[loading,setLoading]=useState(true);
+  const [category,setCategory]=useState(filterData[0].title);
+
+  async function fetchData(){
+    setLoading(true);
+    try{
+      let response=await fetch (apiUrl);
+      let output=await response.json();
+
+      setCourses(output.data);
+      
+    }
+    catch(error){
+       toast.error("Problem in network");
+    }
+    setLoading(false);
+  }
+  //first render 
+  useEffect(()=>{
+    fetchData();
+  },[])
+
+  return (
+    <div className="min-h-screen flex flex-col bg-bgDark2">
+        <div>
+          <Navbar/>
+        </div>
+        <div className="bg-bgDark2">
+          <div>
+              <Filter filterData={filterData} category={category} setCategory={setCategory}/>
+          </div>
+          <div className="w-11/12 flex flex-wrap  mx-auto max-w-[1200px] min-h-[50vh] justify-center items-center">
+            {loading?(<Spinner/>):(<Cards courses={ courses} category={category}/>)}
+          </div>
+
+        </div>
+    </div>
+  );
+};
+
+export default App;
